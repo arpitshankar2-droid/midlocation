5446d167e288468c455c6f9c3cbbf111cf3907d7
Arpit Shankar <arpitshankar2@gmail.com>
2026-04-25 19:30:43 +0530

    fix: update rootDir path for production deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const rootDir = path.resolve(__dirname, '..');
+const rootDir = __dirname;
 const sessionsFile = path.join(__dirname, 'sessions.json');
 let sessions = {};
 
